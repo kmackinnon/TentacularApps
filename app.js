204d2d64f25c6639355eac7bb0413a6815b4444c
Adam Carruthers <adam@bitjutsu.ca>
2014-02-28 21:36:41 -0500

    Add testing server for templating, added jade as dependency

---
+++ app.js
@@ -0,0 +1,26 @@
+#!/bin/env node
+/*
+	Testing server for rendering templates
+	Will NOT run on OpenShift.
+*/
+var express = require('express');
+var jade = require('jade');
+
+/* Initialize express */
+var app = express();
+
+/* Define router before static to avoid static files overriding routes */
+app.use(app.router);
+/* Use static folder for static assets (css, js, img) */
+app.use(express.static('static'));
+
+/* Use Jade as templating engine */
+app.engine('jade', jade.__express);
+
+/* For all URLs, render master.jade for now */
+app.all('*', function(req, res) {
+	res.render('master.jade');
+});
+
+/* Run on port 3000 */
+app.listen(3000, 'localhost');
